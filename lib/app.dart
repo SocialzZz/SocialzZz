d9b53001864e805c51a72f2b86739c31ba4c2d8c
Huynh Van Tai <vawnitaf1307@gmail.com>
2026-01-04 20:26:46 +0700

    update

---
+++ lib/app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+import 'presentation/login_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      title: 'SocialzZz',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFFF6B35)),
+        useMaterial3: true,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
